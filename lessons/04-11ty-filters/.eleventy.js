114ce716e50002606c5f02e00ca72b3933065c00
5t3ph <thinkdobecreate@gmail.com>
2020-09-05 15:12:52 -0500

    Lesson 4 completed

--- lessons/04-11ty-filters/.eleventy.js
+++ lessons/04-11ty-filters/.eleventy.js
@@ -1,7 +1,14 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
-
   eleventyConfig.addPassthroughCopy("./src/css");
+
+  // Filter to retrieve a random blog post
+  eleventyConfig.addFilter("randomPost", (arr) => {
+    arr.sort(() => {
+      return 0.5 - Math.random();
+    });
+    return arr.slice(0, 1);
+  });
 
   return {
     dir: {
