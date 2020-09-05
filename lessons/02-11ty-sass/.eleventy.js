29700456a9200fea1c56aa517bb64bbb0c7f50aa
5t3ph <thinkdobecreate@gmail.com>
2020-09-05 11:45:20 -0500

    Lesson 2 completed

--- lessons/02-11ty-sass/.eleventy.js
+++ lessons/02-11ty-sass/.eleventy.js
@@ -1,4 +1,8 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget("./src/sass/");
+
+  eleventyConfig.addPassthroughCopy("./src/css");
+
   return {
     dir: {
       input: "src",
